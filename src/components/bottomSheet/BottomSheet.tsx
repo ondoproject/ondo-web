a02b86911eb5ab6b87891084c4be7c7b431801d3
beomjo <dongjun1315@naver.com>
2026-01-24 20:28:53 +0900

    fix(bottomsheet): 바텀 시트 리스트 형태 구현

--- src/components/bottomSheet/BottomSheet.tsx
+++ src/components/bottomSheet/BottomSheet.tsx
@@ -59,18 +59,18 @@ export const BottomSheet = ({ locations, onLocationSelect }: BottomSheetProps) =
       {/* Content */}
       <div
         className={cn(
-          'px-5 pb-6 overflow-hidden',
+          'px-5 pb-6 overflow-y-auto',
           'transition-all duration-400',
           isCollapsed ? 'opacity-0 max-h-0 pb-0' : 'opacity-100 max-h-[220px]'
         )}
       >
-        <div className="mb-3.5">
+        <div className="mb-4">
           <p className="text-md text-[var(--accent-taupe)] font-bold">
             광안리 로컬들이 추천해요
           </p>
         </div>
 
-        <div className="flex flex-col gap-3 overflow-x pb-2 scrollbar-thin scrollbar-thumb-gradient">
+        <div className="flex flex-col h-full overflow-y-auto pb-2 scrollbar-thin scrollbar-thumb-gradient">
           {locations.slice(0, 10).map((location) => (
             <PlaceCard
               key={location.id}
--- src/components/bottomSheet/PlaceCard.tsx
+++ src/components/bottomSheet/PlaceCard.tsx
@@ -1,5 +1,6 @@
 import type { Location } from '@/types';
-import { cn } from '@/utils';
+import { cn, transformEngCategory } from '@/utils';
+import { MoveRight } from 'lucide-react';
 
 interface PlaceCardProps {
   location: Location;
@@ -13,27 +14,34 @@ export const PlaceCard = ({ location, onClick }: PlaceCardProps) => {
     <div
       onClick={onClick}
       className={cn(
-        'flex flex-row w-full',
-        'bg-[var(--bg-card)]',
-        'border-t border-b border-[var(--border-color)]',
-        'overflow-hidden',
+        'flex flex-row w-full items-center',
+        'bg-[var(--bg-secondary)]',
+        'border-t border-b border-[var(--text-muted)]',
         'text-left',
-        'transition-all duration-300',
+        'transition-all duration-300 cursor-pointer',
         'hover:shadow-[0_8px_20px_var(--shadow-color)]'
       )}
     >
       <img
         src={location.thumbnail}
         alt={location.name}
-        className="w-[120px] h-[120px] object-cover"
+        className="w-[100px] h-[100px] object-cover"
       />
-      <div className="p-4">
-        <h2 className="text-lg font-semibold text-[var(--text-primary)] truncate">
+      <div className="p-4 flex flex-1 flex-col">
+        <h2 className="text-lg font-bold text-black truncate">
           {location.name}
         </h2>
-        <p className="text-[10px] text-[var(--text-muted)] mt-1">
-          {category}
+        <p className="text-md text-black pb-2 border-b border-[var(--text-muted)]">
+          {transformEngCategory(category)}
         </p>
+        <div className="pt-2 flex justify-between items-center gap-4 text-xs">
+          <p className="text-black font-semibold">
+            {location.address}
+          </p>
+          <div className="flex flex-row items-center gap-1 hover:scale-105 cursor-pointer">
+            Directions <MoveRight size={12}/>
+          </div>
+        </div>
       </div>
     </div>
   );
