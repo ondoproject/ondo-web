60be92059567f3f53c8ccb14b02321c74f034300
beomjo <dongjun1315@naver.com>
2026-02-03 19:52:15 +0900

    feat(constants/categories): 카테 고리 매핑하도록 하는 상수 생성

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -52,3 +52,9 @@ export const getCategoryStyle = (category: string): CategoryStyle => {
 export const getCategoryIcon = (category: string): LucideIcon => {
   return CATEGORY_STYLES[category]?.icon ?? MapPin;
 };
+
+export const CATEGORY_MAP: Record<number, number[]> = {
+  4: [5, 7, 11, 12, 17 ,18, 19, 20],
+  10: [1, 6, 9, 13, 16],
+  14: [2, 3, 15],
+};
